7484c2ac0a35b1217cf41d0a774f87b7ed0600c7
JoshuaPrice1997 <51882009+JoshuaPrice1997@users.noreply.github.com>
2019-06-17 00:40:43 +0930

    Added menu and resizing

--- UofAisBest/test.py
+++ UofAisBest/test.py
@@ -7,38 +7,56 @@ def main():
     # initialize the pygame module
     pygame.init()
     # load and set the logo
-    #logo = pygame.image.load("logo32x32.png")
-    #pygame.display.set_icon(logo)
+    logo = pygame.image.load("logo.png")
+    pygame.display.set_icon(logo)
     pygame.display.set_caption("")
      
     # create a surface on screen that has the size of 240 x 180
-    screen = pygame.display.set_mode((1280,720))
+    screen = pygame.display.set_mode((1280,720),pygame.RESIZABLE)
+    MAXX = 1280
+    MAXY = 720
     x = y = 100
-     
+    state = 0
     # define a variable to control the main loop
     running = True
     player = pygame.image.load('idle1.png').convert()
     background = pygame.image.load('adelaide.jpg').convert()
-    screen.blit(background,(0,0))
-    screen.blit(player,(x,y))
-    print(background)
+    background = pygame.transform.scale(background,(1280,720))
+    welcome = pygame.image.load('welcome.png').convert()
+    enter = pygame.image.load('enter.png').convert()
+    #screen.blit(background,(0,0))
+    #screen.blit(player,(x,y))
     # main loop
     while running:
-        screen.blit(background,(0,0))
-        # event handling, gets all event from the event queue
-        for event in pygame.event.get():
-            # only do something if the event is of type QUIT
-            if event.type == pygame.QUIT:
-                # change the value to False, to exit the main loop
-                running = False
         key = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.VIDEORESIZE:
+            # There's some code to add back window content here.
+                surface = pygame.display.set_mode((event.w, event.h),pygame.RESIZABLE)
+                background = pygame.transform.scale(background,(event.w,event.h))
+                MAXX = event.w
+                MAXY = event.h
+        
+        if state == 0:
+            fontSize = int(MAXY/20)
+            welcome = pygame.transform.scale(welcome,(int(fontSize*42/8),fontSize))
+            enter = pygame.transform.scale(enter,(int(fontSize*42/16),int(fontSize/2)))
+            screen.blit(welcome,(MAXX/2-int(fontSize*42/16),MAXY/2-fontSize/2))
+            screen.blit(enter,(MAXX/2-int(fontSize*42/32),MAXY/2+fontSize*2-fontSize/2))
+            if key[pygame.K_RETURN]:state += 1
+        if state == 1:
+            screen.blit(background,(0,0))
+            screen.blit(player,(x,y))
+        
         if key[pygame.K_w]:y -= 10
         if key[pygame.K_a]:x -= 10
         if key[pygame.K_d]:x += 10
         if key[pygame.K_s]:y += 10
-        screen.blit(player,(x,y))
-        pygame.display.update()
+        
         pygame.time.delay(100)
+        pygame.display.update()
 
      
      
