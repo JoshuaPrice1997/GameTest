f588861bd662e382d0ed803b357898aaed99d788
JoshuaPrice1997 <51882009+JoshuaPrice1997@users.noreply.github.com>
2019-06-18 22:42:34 +0930

    Added Load Functions :cd:

--- UofAisBest/test.py
+++ UofAisBest/test.py
@@ -1,4 +1,6 @@
 import pygame
+import numpy as np
+import pandas as panda
 
 def main():
      
@@ -18,14 +20,17 @@ def main():
     x = y = 100
     #keeps track of game state
     state = 0
-    
     running = True
     #importing assets
+    tiles = loadTiles()
+    text = loadText()
+    print(text)
     player = pygame.image.load('idle1.png').convert()
     background = pygame.image.load('adelaide.jpg').convert()
     background = pygame.transform.scale(background,(1280,720))
-    welcome = pygame.image.load('welcome.png').convert()
-    enter = pygame.image.load('enter.png').convert()
+    mapo = np.loadtxt('map.txt',usecols=range(5))
+
+    graphics = panda.DataFrame(data = {player,background},index=['player','background'])
 
     while running:
         #loading current key presses
@@ -51,8 +56,8 @@ def main():
             #Menu state
             #---------------------#
             fontSize = int(MAXY/20)
-            welcome = pygame.transform.scale(welcome,(int(fontSize*42/8),fontSize))
-            enter = pygame.transform.scale(enter,(int(fontSize*42/16),int(fontSize/2)))
+            welcome = pygame.transform.scale(text[0]['welcome'],(int(fontSize*42/8),fontSize))
+            enter = pygame.transform.scale(text[0]['enter'],(int(fontSize*42/16),int(fontSize/2)))
             screen.blit(welcome,(MAXX/2-int(fontSize*42/16),MAXY/2-fontSize/2))
             screen.blit(enter,(MAXX/2-int(fontSize*42/32),MAXY/2+fontSize*2-fontSize/2))
             #chane to game state
@@ -75,12 +80,38 @@ def main():
         if key[pygame.K_ESCAPE]:sys.exit
 
         #Input Lag
-        pygame.time.delay(100)
+        pygame.time.delay(10)
 
         #Animate
         pygame.display.update()
 
-     
+# def loadGraphics():
+#     player = pygame.image.load('idle1.png').convert()
+#     background = pygame.image.load('adelaide.jpg').convert()
+#     background = pygame.transform.scale(background,(1280,720))
+#     welcome = pygame.image.load('welcome.png').convert()
+#     grass = pygame.transform.scale(pygame.image.load('grass.png').convert(),(128,128))
+#     path = pygame.transform.scale(pygame.image.load('path.png').convert,(128,128))
+#     enter = pygame.image.load('enter.png').convert()
+
+def loadTiles():
+    tiles = panda.DataFrame()
+    tileDat = np.genfromtxt('tiles.txt',usecols=range(2),dtype=str)
+    for i in range(tileDat.shape[0]):
+        temp = panda.DataFrame(data={pygame.transform.scale(pygame.image.load(tileDat[i][1]+'.png').convert(),(128,128))},index=[tileDat[i][1]])
+        tiles = tiles.append(temp)
+    return tiles
+
+def loadText():
+    text = panda.DataFrame()
+    textDat = np.genfromtxt('text.txt',usecols=range(2),dtype=str)
+    print(textDat.shape)
+    for i in range(textDat.shape[0]):
+        temp = panda.DataFrame(data={pygame.image.load(textDat[i][1]+'.png').convert()},index=[textDat[i][1]])
+        text = text.append(temp)
+    return text
+
+
      
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
